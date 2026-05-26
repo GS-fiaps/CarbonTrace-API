c4ff88b5caefc4922c2bc1dd62a0937ff37cd84c
biribillidev <biribillijoaovitor@gmail.com>
2026-05-26 13:10:12 -0300

    feat: Entidade Analise e Enum StatusAlerta concluidos

---
+++ src/main/java/br/com/fiap/carbontrace/enums/StatusAlerta.java
@@ -0,0 +1,8 @@
+package br.com.fiap.carbontrace.enums;
+
+public enum StatusAlerta {
+    NORMAL,
+    ATENCAO,
+    CRITICO,
+    EMERGENCIA
+}
---
+++ src/main/java/br/com/fiap/carbontrace/model/Analise.java
@@ -0,0 +1,35 @@
+package br.com.fiap.carbontrace.model;
+
+import br.com.fiap.carbontrace.enums.StatusAlerta;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "TB_ANALISE")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Analise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAnalise;
+    private LocalDate dataAnalise;
+    private Double areaDesmatadaKm2;
+    private Double percentualVariacao;
+    @Enumerated
+    private StatusAlerta statusAlerta;
+
+    @OneToMany
+    private List<Alerta> alertas;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_analise_imagem")
+    private ImagemSatelital imagemSatelital;
+}
