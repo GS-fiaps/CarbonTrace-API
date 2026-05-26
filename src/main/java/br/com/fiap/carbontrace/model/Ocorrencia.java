cade241d8f4481adf2c0232e9f82135ae3228c98
biribillidev <biribillijoaovitor@gmail.com>
2026-05-26 13:12:06 -0300

    feat: Entidade Ocorrencia concluida

---
+++ src/main/java/br/com/fiap/carbontrace/model/Ocorrencia.java
@@ -0,0 +1,32 @@
+package br.com.fiap.carbontrace.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "TB_OCORRENCIA")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Ocorrencia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idOcorrencia;
+    private LocalDate dataOcorrencia;
+    private String descricao;
+    private Double areaEstimadaKm2;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_ocorrencia_regiao")
+    private Regiao regiao;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_ocorrencia_usuario")
+    private Usuario usuario;
+}
