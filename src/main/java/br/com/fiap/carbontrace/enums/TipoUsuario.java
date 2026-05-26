914d6ca3a46d906a9247f3e0e308f5de4b30c949
biribillidev <biribillijoaovitor@gmail.com>
2026-05-26 13:12:47 -0300

    feat: Entidade Usuario e Enum TipoUsuario concluidos

---
+++ src/main/java/br/com/fiap/carbontrace/enums/TipoUsuario.java
@@ -0,0 +1,7 @@
+package br.com.fiap.carbontrace.enums;
+
+public enum TipoUsuario {
+    ADMIN,
+    ANALISATA,
+    FISCAL
+}
---
+++ src/main/java/br/com/fiap/carbontrace/model/Usuario.java
@@ -0,0 +1,35 @@
+package br.com.fiap.carbontrace.model;
+
+import br.com.fiap.carbontrace.enums.TipoUsuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "TB_USUARIO")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+    private String nome;
+    private String email;
+    private String senha;
+    @Enumerated
+    private TipoUsuario tipoUsuario;
+    private LocalDate dataCadastro;
+
+    @OneToMany
+    private List<Ocorrencia> ocorrencias;
+
+    @OneToMany
+    private List<Relatorio> relatorios;
+}
