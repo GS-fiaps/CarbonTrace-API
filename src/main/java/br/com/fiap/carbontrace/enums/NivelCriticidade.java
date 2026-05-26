06bbd83d4bb71b060854a060726441cd9bf38c1c
biribillidev <biribillijoaovitor@gmail.com>
2026-05-26 13:10:41 -0300

    feat: Entidade Alerta e Enum NivelCriticidade concluidos

---
+++ src/main/java/br/com/fiap/carbontrace/enums/NivelCriticidade.java
@@ -0,0 +1,8 @@
+package br.com.fiap.carbontrace.enums;
+
+public enum NivelCriticidade {
+    BAIXO,
+    MEDIO,
+    ALTO,
+    CRITICO
+}
---
+++ src/main/java/br/com/fiap/carbontrace/model/Alerta.java
@@ -0,0 +1,34 @@
+package br.com.fiap.carbontrace.model;
+
+import br.com.fiap.carbontrace.enums.NivelCriticidade;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "TB_ALERTA")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Alerta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAlerta;
+    private LocalDate dataEmissao;
+    @Enumerated
+    private NivelCriticidade nivelCriticidade;
+    private String descricao;
+
+    @OneToMany
+    private List<AlertaOrgao> alertasOrgaos;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_alerta_analise")
+    private Analise analise;
+}
