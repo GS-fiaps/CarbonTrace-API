0af8c062d1b72c1bb1c3ec3b885a4862a1de6fbb
biribillidev <biribillijoaovitor@gmail.com>
2026-05-26 12:14:22 -0300

    feat: Entidades Regiao e Estado criadas

--- src/main/java/br/com/fiap/carbontrace/model/Estado.java
+++ src/main/java/br/com/fiap/carbontrace/model/Estado.java
@@ -1,4 +1,25 @@
 package br.com.fiap.carbontrace.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "Estado")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Estado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idEstado;
+    private String nome;
+    private String sigla;
+    private List <Regiao>regioes ;
+    private List <OrgaoAmbiental> orgaosAmbientais;
 }
--- src/main/java/br/com/fiap/carbontrace/model/Regiao.java
+++ src/main/java/br/com/fiap/carbontrace/model/Regiao.java
@@ -1,4 +1,27 @@
 package br.com.fiap.carbontrace.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "Regiao")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Regiao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idRegiao;
+    private String nome;
+    private Double latitude;
+    private Double areaKmQuadrado;
+    private Estado estado;
+    private List <ImagemSatelital> imagensSatelitais;
+    private List <Ocorrencia> ocorrencias;
 }
