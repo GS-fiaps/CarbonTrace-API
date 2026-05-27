b18a2fb1cc4418ecd4eae1c12656d45318fc7182
biribillidev <biribillijoaovitor@gmail.com>
2026-05-27 10:30:25 -0300

    feat: OrgaoAmbientalRepository concluido

---
+++ src/main/java/br/com/fiap/carbontrace/repositories/OrgaoAmbientalRepository.java
@@ -0,0 +1,7 @@
+package br.com.fiap.carbontrace.repositories;
+
+import br.com.fiap.carbontrace.model.OrgaoAmbiental;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrgaoAmbientalRepository extends JpaRepository<OrgaoAmbiental,Long> {
+}
