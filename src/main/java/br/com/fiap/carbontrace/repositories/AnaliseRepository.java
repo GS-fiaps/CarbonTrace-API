a00de55f7541bbfe920fba98264cba197c5cd23a
biribillidev <biribillijoaovitor@gmail.com>
2026-05-27 10:27:25 -0300

    feat: AnaliseRepository concluido

---
+++ src/main/java/br/com/fiap/carbontrace/repositories/AnaliseRepository.java
@@ -0,0 +1,7 @@
+package br.com.fiap.carbontrace.repositories;
+
+import br.com.fiap.carbontrace.model.Analise;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnaliseRepository extends JpaRepository<Analise,Long> {
+}
