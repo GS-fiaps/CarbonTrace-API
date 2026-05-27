6c4a4b265f4b9c0d84993dedad769e765b72d1db
biribillidev <biribillijoaovitor@gmail.com>
2026-05-27 10:24:38 -0300

    feat: RegiaoRepository concluido

---
+++ src/main/java/br/com/fiap/carbontrace/repositories/RegiaoRepository.java
@@ -0,0 +1,7 @@
+package br.com.fiap.carbontrace.repositories;
+
+import br.com.fiap.carbontrace.model.Regiao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegiaoRepository extends JpaRepository <Regiao, Long> {
+}
